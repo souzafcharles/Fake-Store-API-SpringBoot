8d16ded65d6d69dceae8944b889316c9f06e5fbf
souzafcharles <souzach@outlook.com>
2025-09-09 11:40:32 -0300

    feat: add WebClient with FakeStore API integration

---
+++ src/main/java/com/github/souzafcharles/api/client/FakeStoreClient.java
@@ -0,0 +1,30 @@
+package com.github.souzafcharles.api.client;
+
+import com.github.souzafcharles.api.model.dto.ProductResponseDTO;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+@Component
+public class FakeStoreClient {
+
+    private final WebClient webClient;
+
+    public FakeStoreClient(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    public Mono<ProductResponseDTO[]> getAllProducts() {
+        return webClient.get()
+                .uri("https://fakestoreapi.com/products")
+                .retrieve()
+                .bodyToMono(ProductResponseDTO[].class);
+    }
+
+    public Mono<ProductResponseDTO> getProductById(Long id) {
+        return webClient.get()
+                .uri("https://fakestoreapi.com/products/{id}", id)
+                .retrieve()
+                .bodyToMono(ProductResponseDTO.class);
+    }
+}
