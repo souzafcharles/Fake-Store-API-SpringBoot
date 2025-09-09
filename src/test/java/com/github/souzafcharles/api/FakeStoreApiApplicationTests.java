a74d72c2a02fee01bca7082b1011d7e1fecb4357
souzafcharles <souzach@outlook.com>
2025-09-09 10:30:48 -0300

    chore: initial project setup

---
+++ src/test/java/com/github/souzafcharles/api/FakeStoreApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.souzafcharles.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FakeStoreApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
